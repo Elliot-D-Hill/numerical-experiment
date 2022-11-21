18dd783e088c6b9910555889fd1989c800495487
Elliot Hill <elliot.douglas.hill@gmail.com>
2022-11-21 00:51:39 -0500

    Add interactions

--- src/studydesign/experiment.py
+++ src/studydesign/experiment.py
@@ -1,3 +1,4 @@
+from itertools import product
 from numpy import atleast_1d
 from pandas import DataFrame
 from typing import Callable
@@ -13,29 +14,40 @@ def exclude_variables(variables: dict, exclude: list) -> dict:
     }
 
 
-def format_results(results: dict) -> DataFrame:
-    results = DataFrame(results)
-    columns = results.columns.tolist()
-    return results[columns[-2:] + columns[:-2]]
+def make_interactions(interactions, experiment, variables, fixed_parameters):
+    interaction_parameters = []
+    for name_a, name_b in interactions:
+        for a, b in product(variables[name_a], variables[name_b]):
+            parameters = deepcopy(fixed_parameters)
+            parameters[name_a] = a
+            parameters[name_b] = b
+            parameters = (
+                {"variable": name_a + "_" + name_b}
+                | parameters
+                | experiment(**parameters)
+            )
+            interaction_parameters.append(parameters)
+    return interaction_parameters
 
 
 def run_experiment(
     experiment: Callable,
     variables: dict,
     fixed_parameters: dict,
+    interactions: list[tuple[str]] = None,
     exclude: list[str] = None,
 ) -> DataFrame:
     variables = exclude_variables(variables, exclude)
     results = []
     for name, variable in variables.items():
-        variable = atleast_1d(variable)
-        n_values = len(variable)
-        values = enumerate(variable)
-        for run, value in tqdm(values, total=n_values, desc=name):
+        for run, value in tqdm(atleast_1d(variable), total=len(variable), desc=name):
             parameters = deepcopy(fixed_parameters)
             parameters[name] = value
-            parameters = parameters | experiment(**parameters)
-            parameters["variable"] = name
-            parameters["run"] = run + 1
+            parameters = {"variable": name} | parameters | experiment(**parameters)
             results.append(parameters)
-    return format_results(results)
+    if interactions is not None:
+        iteraction_results = make_interactions(
+            interactions, experiment, variables, fixed_parameters
+        )
+        results = results + iteraction_results
+    return DataFrame(results)
